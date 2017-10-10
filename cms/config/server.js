12a5b1727c63761bf80ec7e2a8a52835131bf553
leobds <leonardo.bds.desenv@gmail.com>
2017-10-10 18:07:42 -0300

    alterações

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,58 +1,27 @@
-/* importar o módulo do framework express */
 var express = require('express');
-
-/* importar o módulo do consign */
 var consign = require('consign');
-
-/* importar o módulo do body-parser */
 var bodyParser = require('body-parser');
-
-/* importar o módulo do express-validator */
 var expressValidator = require('express-validator');
 
-/* iniciar o objeto do express */
 var app = express();
-
-/* setar as variáveis 'view engine' e 'views' do express */
+//var mensagem = require('./modulo_teste');
+// A engine de view mudou. Será  EJS e para setarmos isso na propriedade do node
+// iremos utilizar:
 app.set('view engine', 'ejs');
-<<<<<<< HEAD
-app.set('views', '.app/views');
-
-app.use(express.static('./app/public'));
-
-app.use(bodyParser.urlencoded({extend: true}));
-=======
 app.set('views', './app/views');
 
-/* configurar o middleware express.static */
 app.use(express.static('./app/public'));
 
-/* configurar o middleware body-parser */
-app.use(bodyParser.urlencoded({extended: true}));
-
-/* configurar o middleware express-validator */
->>>>>>> 087665eea51654285e946775d97abcd6f6241eb1
+app.use(bodyParser.urlencoded({extend: false}));
 app.use(expressValidator());
 
-/* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()
-<<<<<<< HEAD
 .include('app/routes')
 .then('config/dbConnection.js')
-.then('app/models')
+//.then('app/models')
 .then('app/controllers')
 .into(app);
 
+
+
 module.exports = app;
-
-
-
-=======
-	.include('app/routes')
-	.then('app/models')
-	.then('app/controllers')
-	.into(app);
-
-/* exportar o objeto app */
-module.exports = app;
->>>>>>> 087665eea51654285e946775d97abcd6f6241eb1
